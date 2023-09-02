e6eebe077489fee07f1196f4276d4b04d2ef3a7e
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2023-09-02 17:46:51 +1000

    build error resolved

--- app/(sites)/profiles/page.tsx
+++ app/(sites)/profiles/page.tsx
@@ -3,14 +3,15 @@ import React from "react";
 import { AddProfile, ProfileCard } from "@/components";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+
 const Profiles = async () => {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/login");
   }
   return (
-    <div className="h-[100vh] flex flex-col items-center justify-center ">
-      <p className="text-5xl mb-8">Who's Watching?</p>
+    <div className="h-[100vh] flex flex-col items-center justify-center">
+      <p className="text-5xl mb-8">Who&apos;s Watching?</p>
       <div className=" flex gap-6">
         <ProfileCard />
 
