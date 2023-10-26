96b18d1dd3042c65f6922bfe2c017c436119da4c
Rauniyar.shovit <rauniyar.shovit62@gmail.com>
2023-10-26 17:06:26 +1100

    profile bug

--- utils/fetchProfiles.ts
+++ utils/fetchProfiles.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchProfiles(
   email: string,
-  path: string = "http://api/fetchProfiles"
+  path: string = "/api/fetchProfiles"
   // path: string = "http://127.0.0.1:3000/api/fetchProfiles"
 ) {
   try {
